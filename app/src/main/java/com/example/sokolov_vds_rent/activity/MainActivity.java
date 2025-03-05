7dc29077b9bb9fb7e7240ece13211e924766408c
sokolov <speedvagon1991@gmail.ru>
2025-03-05 21:47:04 +0300

    Added MainActivity Fragments Commit

--- app/src/main/java/com/example/sokolov_vds_rent/activity/MainActivity.java
+++ app/src/main/java/com/example/sokolov_vds_rent/activity/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.sokolov_vds_rent.activity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import com.example.sokolov_vds_rent.R;
 import com.example.sokolov_vds_rent.databinding.ActivityMainBinding;
+import com.example.sokolov_vds_rent.fragment.AboutAppFragment;
+import com.example.sokolov_vds_rent.fragment.AboutDevFragment;
+import com.example.sokolov_vds_rent.fragment.HomeFragment;
+import com.example.sokolov_vds_rent.fragment.InstructionsFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,15 +30,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.home){
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, HomeFragment.class, null).commit();
                 } else if (item.getItemId() == R.id.instuctions) {
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, InstructionsFragment.class, null).commit();
                 } else if (item.getItemId() == R.id.about_app) {
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, AboutAppFragment.class, null).commit();
                 } else if (item.getItemId() == R.id.about_developer) {
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, AboutDevFragment.class, null).commit();
                 }
-                return false;
+                binding.main.closeDrawer(GravityCompat.START);
+                return true;
+
             }
         });
 
