7edc7b8255785a34c340d1e89a811c9f49542b4b
sokolov <speedvagon1991@gmail.ru>
2025-03-05 20:59:14 +0300

    Added MainActivity Fragments Commit

--- app/src/main/java/com/example/sokolov_vds_rent/activity/MainActivity.java
+++ app/src/main/java/com/example/sokolov_vds_rent/activity/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.sokolov_vds_rent.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sokolov_vds_rent.R;
+import com.example.sokolov_vds_rent.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home){
+
+                } else if (item.getItemId() == R.id.instuctions) {
+
+                } else if (item.getItemId() == R.id.about_app) {
+
+                } else if (item.getItemId() == R.id.about_developer) {
+
+                }
+                return false;
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/sokolov_vds_rent/fragment/AboutAppFragment.java
@@ -0,0 +1,28 @@
+package com.example.sokolov_vds_rent.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sokolov_vds_rent.R;
+
+
+public class AboutAppFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_app, container, false);
+    }
+}
